078ca473d2bb9d349cc3a772e41f39760052b660
Vadim <thecruly@gmail.com>
2026-04-11 02:28:04 +0900

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.net.ServerSocket;
-import java.net.Socket;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args){
@@ -8,41 +8,40 @@ public class Main {
         System.out.println("Logs from your program will appear here!");
 
         // Uncomment the code below to pass the first stage
-        Socket clientSocket = null;
         int port = 6379;
         try (var serverSocket = new ServerSocket(port)) {
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
-            clientSocket = serverSocket.accept();
-            System.out.println("Client is connected");
-            try(var clientInputReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                var serverOutputWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))) {
-                while (clientSocket.isConnected()) {
-                    String inputLine = clientInputReader.readLine();
-                    if ("PING".equals(inputLine)) {
-                        serverOutputWriter.write("+PONG\r\n");
-                        serverOutputWriter.flush();
-                    } else {
-                        System.out.println("Unexpected input:");
-                        System.out.println(inputLine);
-                    }
+            try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+                while (true) {
+                    var clientSocket = serverSocket.accept();
+                    executor.submit(() -> {
+                        System.out.println("Client is connected");
+                        try (var clientInputReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                             var serverOutputWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))) {
+                            while (clientSocket.isConnected()) {
+                                String inputLine = clientInputReader.readLine();
+                                if ("PING".equals(inputLine)) {
+                                    serverOutputWriter.write("+PONG\r\n");
+                                    serverOutputWriter.flush();
+                                } else {
+                                    System.out.println("Unexpected input:");
+                                    System.out.println(inputLine);
+                                }
 
+                            }
+                            System.out.println("Client is disconnected");
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
                 }
-                System.out.println("Client is disconnected");
             }
 //            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-        } finally {
-            try {
-                if (clientSocket != null) {
-                    clientSocket.close();
-                }
-            } catch (IOException e) {
-                System.out.println("IOException: " + e.getMessage());
-            }
         }
     }
 }
