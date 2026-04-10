cb9008f171ec826b6ee0dc8248f92b0dc27be743
Vadim <thecruly@gmail.com>
2026-04-11 01:18:21 +0900

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,17 +8,34 @@ public class Main {
         System.out.println("Logs from your program will appear here!");
 
         // Uncomment the code below to pass the first stage
-        ServerSocket serverSocket = null;
         Socket clientSocket = null;
         int port = 6379;
-        try {
-            serverSocket = new ServerSocket(port);
+        try (var serverSocket = new ServerSocket(port)) {
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
-            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            System.out.println("Client is connected");
+            try(var clientInputReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                var serverOutputWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))) {
+                while (clientSocket.isConnected()) {
+                    String inputLine = clientInputReader.readLine();
+                    System.out.println(inputLine);
+                    if ("PING".equals(inputLine)) {
+                        serverOutputWriter.write("+PONG\r\n");
+                        serverOutputWriter.flush();
+                    } else {
+                        System.out.println("Unexpected input:");
+                        System.out.println(inputLine);
+                        serverOutputWriter.write("Unexpected input:" + inputLine + "\n");
+                        serverOutputWriter.flush();
+                    }
+
+                }
+                System.out.println("Client is disconnected");
+            }
+//            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
